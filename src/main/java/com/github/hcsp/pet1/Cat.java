f87c617be4e3bff3e247a56d7f49b0c32502cc3d
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-06-19 22:38:46 +0800

    create two class in diffrenent package (#29)
    create two class in diffrenent package (#29)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,7 @@
+package com.github.hcsp.pet1;
+
+public class Cat {
+  public static void main(String[] args) {
+    System.out.println("Dog class");
+  }
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,7 @@
+package com.github.hcsp.pet2;
+
+public class Dog {
+  public static void main(String[] args) {
+    System.out.println("Dog class");
+  }
+}
