22e47f7fda985cb2cde3ae02b31969ed77819dee
Lian Yongbo <L1anyX@126.com>
2019-10-16 19:05:03 +0800

    分别在pet1和pet2包下创建了Cat类和Dog类 (#116)
    分别在pet1和pet2包下创建了Cat类和Dog类 (#116)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.pet1;
+
+/**
+ * @Auther:Lian
+ * @Date:2019/10/16 19:02
+ * @Description:TODO
+ * @version:1.0
+ */
+public class Cat {
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.pet2;
+
+/**
+ * @Auther:Lian
+ * @Date:2019/10/16 19:02
+ * @Description:TODO
+ * @version:1.0
+ */
+public class Dog {
+}
