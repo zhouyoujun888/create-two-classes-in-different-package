0d5319c05a2fe62979f8fc1758abefa4be857362
Nick Yang <kc.yang@foxmail.com>
2020-03-16 10:56:09 +0800

    在不同包中创建类 (#203)
    在不同包中创建类 (#203)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.pet1;
+
+/**
+ * @author: yangjianrong
+ * @create: 2020/3/16 10:51
+ * @description:
+ */
+
+public class Cat {
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.pet2;
+
+/**
+ * @author: yangjianrong
+ * @create: 2020/3/16 10:52
+ * @description:
+ */
+
+public class Dog {
+}
