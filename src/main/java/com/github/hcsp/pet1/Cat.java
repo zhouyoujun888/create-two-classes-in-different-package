784959c1aa202ce7824a4bd8b28e9b91d663b03d
gofocus <go_focus@live.cn>
2020-03-18 12:50:00 +0800

    在不同的包中创建两个类 (#205)
    在不同的包中创建两个类 (#205)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,9 @@
+package com.github.hcsp.pet1;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-03-18 12:46
+ * @Description:
+ */
+public class Cat {
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,9 @@
+package com.github.hcsp.pet2;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-03-18 12:47
+ * @Description:
+ */
+public class Dog {
+}
