f25a34e7558e767a88d5877adc4240a23a43ebb0
左佳宾 <59503711+keda725@users.noreply.github.com>
2020-06-03 14:25:18 +0800

    创建指定包中指定的类 (#238)
    创建指定包中指定的类 (#238)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,7 @@
+package com.github.hcsp.pet1;
+
+/**
+ * create by crashLab on 2020/06/03.
+ **/
+public class Cat {
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,7 @@
+package com.github.hcsp.pet2;
+
+/**
+ * create by crashLab on 2020/06/03.
+ **/
+public class Dog {
+}
