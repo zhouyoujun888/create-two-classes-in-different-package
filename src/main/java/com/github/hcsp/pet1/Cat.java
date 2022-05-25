fd945ea9237623bdd29d95fe84116bc3ad7916e3
lei <106215839+3stonelei@users.noreply.github.com>
2022-05-25 23:28:21 +0800

    在不同的包下创建两个类 Cat和Dog (#353)
    在不同的包下创建两个类 Cat和Dog (#353)

---
+++ src/main/java/com/github/hcsp/pet1/Cat.java
@@ -0,0 +1,8 @@
+package com.github.hcsp.pet1;
+
+/**
+ * @author Lilei
+ * @date 2022/5/25-@23:25
+ */
+public class Cat {
+}
---
+++ src/main/java/com/github/hcsp/pet2/Dog.java
@@ -0,0 +1,8 @@
+package com.github.hcsp.pet2;
+
+/**
+ * @author Lilei
+ * @date 2022/5/25-@23:26
+ */
+public class Dog {
+}
